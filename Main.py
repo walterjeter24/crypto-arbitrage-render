e4f5face96768dd170129d6703597062005508d7
walterjeter24 <144391355+walterjeter24@users.noreply.github.com>
2025-06-26 14:22:06 -0400

    Create Main.py
    ARb bot

---
+++ Main.py
@@ -0,0 +1,12 @@
+print("🔁 Arbitrage bot started...")
+
+# Example simulation
+token = "ETH"
+buy_price = 2439.51
+sell_price = 2459.67
+net_profit = sell_price - buy_price
+
+print(f"Token: {token}")
+print(f"Buy Price: ${buy_price}")
+print(f"Sell Price: ${sell_price}")
+print(f"Net Profit: ${net_profit}")
